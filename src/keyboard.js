b645c00c1a907eb3f2b3030abc7cf417c387a6b1
Leo Cassarani <leo.cassarani@me.com>
2015-03-24 10:36:29 +0000

    Add missing key codes to the Keyboard module
    Adds Home, End, Insert, Page Up/Down, and the Function Keys F1 - F12.

--- src/keyboard.js
+++ src/keyboard.js
@@ -18,13 +18,26 @@ function Keyboard() {
   };
 
   function translateKeyCode(keyCode) {
+    // Function Keys: F1 - F12
+    if (keyCode >= 112 && keyCode <= 123) {
+      return keyCode + 29;
+    }
+
     switch (keyCode) {
       case 8:
         return 129; // Backspace
-      case 27:
-        return 140; // Escape
       case 13:
         return 128; // Return
+      case 27:
+        return 140; // Escape
+      case 33:
+        return 136; // Page Up
+      case 34:
+        return 137; // Page Down
+      case 35:
+        return 135; // End
+      case 36:
+        return 134; // Home
       case 37:
         return 130; // Left arrow
       case 38:
@@ -33,6 +46,8 @@ function Keyboard() {
         return 132; // Right arrow
       case 40:
         return 133; // Down arrow
+      case 45:
+        return 138; // Insert
       case 46:
         return 139; // Delete
       default:
